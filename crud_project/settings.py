c0c823873507a822ef2b8794eb1136a73a3c1310
Valeska Chica <valeskasofiachica02@gmail.com>
2025-07-02 15:18:14 -0500

    Host

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'replace-this-with-a-secure-key'
+SECRET_KEY = 'pass111'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
