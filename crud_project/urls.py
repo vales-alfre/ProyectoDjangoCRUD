192d6fd8616ef2060305e0a5586d9a24a2d7fef6
Valeska Chica <valeskasofiachica02@gmail.com>
2025-07-02 16:10:51 -0500

    uj

--- crud_project/urls.py
+++ crud_project/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse("OK")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',health, name='health'),
     path('', RedirectView.as_view(url='/items/', permanent=False)),
     path('items/', include('crud.urls')),
 ]
