7e392b6d6f616ec3d4313db4e39babcac524b099
Valeska Chica <valeskasofiachica02@gmail.com>
2025-07-02 17:18:09 -0500

    Update settings.py

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'crud_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
